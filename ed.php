46a3129bce56bf6020c0d5b34dc9b22269a5da6e
Alengeorge-01 <40091660+Alengeorge-01@users.noreply.github.com>
2025-06-08 15:35:31 +1000

    Refactor edit handler and ensure table names are used

--- ed.php
+++ ed.php
@@ -1,90 +1,53 @@
 <?php
-	require_once('connect.php');
-	$table=$_REQUEST['tablename'];	
-	$name=$_REQUEST['name'];	
-	$productid=$_REQUEST['productid'];	
-	$image=$_REQUEST['image'];	
-	$price=$_REQUEST['price'];	
-	$si=$_REQUEST['size'];	
-	$description=$_REQUEST['description'];	
+require_once('connect.php');
 
+$table       = $_REQUEST['tablename'];
+$name        = $_REQUEST['name'];
+$productid   = $_REQUEST['productid'];
+$image       = $_REQUEST['image'];
+$price       = $_REQUEST['price'];
+$si          = $_REQUEST['size'];
+$description = $_REQUEST['description'];
 
-	
-	$query = "select * from $table where product_id='$productid'";
-	$dup=mysqli_query($link,$query);
-		if( mysqli_num_rows($dup)==0)  { 
-		    echo "<script> alert('Product ID doesnt exist.');
-                window.location='edit.php';
-             </script>";  
-		}  
-	    else { 
-            if($table=='mobile'){
-                        $query1 = "update mobile set mname='$name' where product_id='$productid' ";
-                $query2 = "update mobile set image='$image' where product_id='$productid' ";
-                $query3 = "update mobile set size='$si' where product_id='$productid' ";
-                $query4 = "update mobile set description='$description' where product_id='$productid' ";
-                $query5 = "update mobile set price=$price where product_id='$productid' ";
-				$result1 = mysqli_query($link,$query1);
-                $result2 = mysqli_query($link,$query2);
-                $result3 = mysqli_query($link,$query3);
-                $result4 = mysqli_query($link,$query4);
-                $result5 = mysqli_query($link,$query5);
-                echo "<script>
-                	window.location='mobile.php';
-                </script>";
-            }
-            else if($table=='laptop'){
-                        $query1 = "update laptop set lname='$name' where product_id='$productid' ";
-                $query2 = "update laptop set image='$image' where product_id='$productid' ";
-                $query3 = "update laptop set size='$si' where product_id='$productid' ";
-                $query4 = "update laptop set description='$description' where product_id='$productid' ";
-                $query5 = "update laptop set price=$price where product_id='$productid' ";
-                $result1 = mysqli_query($link,$query1);
-                $result2 = mysqli_query($link,$query2);
-                $result3 = mysqli_query($link,$query3);
-                $result4 = mysqli_query($link,$query4);
-                $result5 = mysqli_query($link,$query5);
-                echo "<script>
-                    window.location='laptop.php';
-                </script>";
-            }
-            else if($table=='watch'){
-                        $query1 = "update watch set wname='$name' where product_id='$productid' ";
-                $query2 = "update watch set image='$image' where product_id='$productid' ";
-                $query3 = "update watch set size='$si' where product_id='$productid' ";
-                $query4 = "update watch set description='$description' where product_id='$productid' ";
-                $query5 = "update watch set price=$price where product_id='$productid' ";
-                $result1 = mysqli_query($link,$query1);
-                $result2 = mysqli_query($link,$query2);
-                $result3 = mysqli_query($link,$query3);
-                $result4 = mysqli_query($link,$query4);
-                $result5 = mysqli_query($link,$query5);
-                echo "<script>
-                    window.location='watch.php';
-                </script>";
-            }
-            else if($table=='television'){
-                        $query1 = "update television set tname='$name' where product_id='$productid' ";
-                $query2 = "update television set image='$image' where product_id='$productid' ";
-                $query3 = "update television set size='$si' where product_id='$productid' ";
-                $query4 = "update television set description='$description' where product_id='$productid' ";
-                $query5 = "update television set price=$price where product_id='$productid' ";
-                $result1 = mysqli_query($link,$query1);
-                $result2 = mysqli_query($link,$query2);
-                $result3 = mysqli_query($link,$query3);
-                $result4 = mysqli_query($link,$query4);
-                $result5 = mysqli_query($link,$query5);
-                echo "<script>
-                    window.location='tv.php';
-                </script>";
-            }
-            else{
-                echo "<script>
-                        alert('No Table Exists');
-                		window.location='edit.php';
-                    </script>";
-                }
-            }
+$nameColumns = [
+    'mobile'     => 'mname',
+    'laptop'     => 'lname',
+    'watch'      => 'wname',
+    'television' => 'tname'
+];
+
+$redirects = [
+    'mobile'     => 'mobile.php',
+    'laptop'     => 'laptop.php',
+    'watch'      => 'watch.php',
+    'television' => 'tv.php'
+];
+
+$query = "select * from $table where product_id='$productid'";
+$dup   = mysqli_query($link, $query);
+
+if (mysqli_num_rows($dup) == 0) {
+    echo "<script> alert('Product ID doesnt exist.'); window.location='edit.php'; </script>";
+} elseif (isset($nameColumns[$table])) {
+    $nameCol = $nameColumns[$table];
+
+    $query1 = "update $table set $nameCol='$name' where product_id='$productid'";
+    $query2 = "update $table set image='$image' where product_id='$productid'";
+    $query3 = "update $table set size='$si' where product_id='$productid'";
+    $query4 = "update $table set description='$description' where product_id='$productid'";
+    $query5 = "update $table set price=$price where product_id='$productid'";
+
+    mysqli_query($link, $query1);
+    mysqli_query($link, $query2);
+    mysqli_query($link, $query3);
+    mysqli_query($link, $query4);
+    mysqli_query($link, $query5);
+
+    $redirect = $redirects[$table];
+    echo "<script> window.location='$redirect'; </script>";
+} else {
+    echo "<script> alert('No Table Exists'); window.location='edit.php'; </script>";
+}
 ?>
 
 
